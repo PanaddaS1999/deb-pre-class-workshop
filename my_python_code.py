28906054e212a2c14ceb5da8d5c57ccf340b57e8
PanaddaS1999 <p.sangklaew@gmail.com>
2026-08-08 17:20:50 +0000

    Add my code to repo

---
+++ my_python_code.py
@@ -0,0 +1,21 @@
+temperatures = [35, 25, 26, 25, 29, 27, 38] 
+ 
+total = sum(temperatures)
+average = total / len(temperatures)
+print(average)
+
+for i , t in enumerate(temperatures):
+    print(f"วันที่ {i} มีค่า {t} องศา")
+
+def classify_temp(t, avg):
+    if t > avg:
+        return "ร้อน"
+    else:
+        return "เย็น"
+
+print(classify_temp(30, average))
+
+import pandas as pd 
+
+df = pd.read_csv("pokemon.csv")
+print(df.head())
